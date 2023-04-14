a813d7b2cc7538d06b960d1ef1675e163fdad656
Vairavan Udayappan <vairavan.udayappan@gmail.com>
2023-04-13 23:54:03 -0600

    firebase data-fetch

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { AuthContextProvider } from '@/context/AuthContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <AuthContextProvider>
+        <Component {...pageProps} />
+      </AuthContextProvider>
+    </>
+  );
 }
 
 export default MyApp
